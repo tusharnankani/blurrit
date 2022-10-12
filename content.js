0441a976dab5e4c6b86eb55c00e9d0e3e0f66b0b
Tushar Nankani <tusharnankani3@gmail.com>
2022-10-12 11:38:17 +0530

    :pencil: docs: add issues with prev. implementation

--- content.js
+++ content.js
@@ -2,6 +2,13 @@
 // let items = document.getElementsByClassName("message-in");
 // let whatsappMessageTexts = document.querySelectorAll("div.copyable-text");
 
+/*
+    Multiple problems
+    0. Blurs <forwarded> instead of name.
+    1. Doesn't blur names for above a <sent media>; because of `.copyable-text`.
+    2. Doesn't blur names in tagged messages.
+*/ 
+
 // for (let text of whatsappMessageTexts) {
 //     let textSibling = text.parentElement.firstChild;
 //     if(textSibling != text) {
@@ -16,7 +23,6 @@ blurIt = () => {
     
     let header = document.getElementsByTagName('header')[1];
     header ? header.style.filter = "blur(4px)" : " ";
-    
 
     for(i = 0; i < 256; ++i) {
         let potentialListOfUserNameColors = document.getElementsByClassName(`color-${i+1}`);
