d9924088572b829fc3479cd1147060badb10317e
Jay-Kishn <76587504+Jay-Kishn@users.noreply.github.com>
2022-12-06 22:57:16 +0530

    add feature to blur messages by default and unblur when clicked

--- content.js
+++ content.js
@@ -10,6 +10,14 @@ blurIt = () => {
     let header = document.getElementsByTagName('header')[1];
     header ? header.classList.add("blurrit-class", "blur-4") : "";
 
+    const elements = document.querySelector('#pane-side > div:nth-child(1) > div > div ');
+    const len = elements.childElementCount;
+
+    for(i = 0;i<len;i++){
+        const element = document.querySelector('#pane-side > div:nth-child(1) > div > div > div:nth-child('+i+') > div > div > div > div._3OvU8 > div._37FrU');
+        element? element.classList.add("blurrit-class", "blur-4") : "";
+    }
+
     for(i = 0; i < 256; ++i) {
         let potentialListOfUserNameColors = document.getElementsByClassName(`color-${i+1}`);
         let potentialListOfUserNameBgColors = document.getElementsByClassName(`bg-color-${i+1}`);
