74e086238d0408c06b6f1740f3ad7c194a5e6f58
tusharnankani <tusharnankani3@gmail.com>
2022-12-06 13:24:42 +0530

    :truck: ship: v1.0 (add err handling, hotkey, publish)

--- background.js
+++ background.js
@@ -1,48 +1,56 @@
 let toggle = false;
 
-chrome.action.onClicked.addListener((tab) => {
+try {
+    chrome.action.onClicked.addListener((tab) => {
 
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ['content.js']
-    })
+        if(tab.url.match("https://web.whatsapp.com/*") !== null) {
 
-    toggle = !toggle;
-
-    toggle ? (
-        chrome.scripting.insertCSS({
-            target: {tabId: tab.id},
-            css: `.blurrit-class {
-                transition: all 0.3s ease;
-            }
-            
-            .blur-10 {
-                filter: blur(10px);
-            }
-            
-            .blur-4 {
-                filter: blur(4px);
-            }
-            
-            .blurrit-class:hover {
-                filter: none;
-            }`
-        })
-        ) : (
-            chrome.scripting.insertCSS({
+            chrome.scripting.executeScript({
                 target: {tabId: tab.id},
-                css: `.blurrit-class {
-                    transition: none;
-                }
-                
-                .blur-10 {
-                    filter: none;
-                }
-                
-                .blur-4 {
-                    filter: none;
-                }`
-            })
-    );
-    
-});
+                files: ['content.js']
+            });
+        
+            toggle = !toggle;
+        
+            toggle ? (
+                chrome.scripting.insertCSS({
+                    target: {tabId: tab.id},
+                    css: `.blurrit-class {
+                        transition: all 0.3s ease;
+                    }
+                    
+                    .blur-10 {
+                        filter: blur(10px);
+                    }
+                    
+                    .blur-4 {
+                        filter: blur(4px);
+                    }
+                    
+                    .blurrit-class:hover {
+                        filter: none;
+                    }`
+                })
+                ) : (
+                    chrome.scripting.insertCSS({
+                        target: {tabId: tab.id},
+                        css: `.blurrit-class {
+                            transition: none;
+                        }
+                        
+                        .blur-10 {
+                            filter: none;
+                        }
+                        
+                        .blur-4 {
+                            filter: none;
+                        }`
+                    })
+            );
+
+        }
+
+    });
+} catch (err) {
+    console.log("Outside: ", err);
+}
