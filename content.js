12aa00f350386f5dea29338788d607c61973c779
Restyled.io <commits@restyled.io>
2023-02-13 16:40:58 +0000

    Restyled by clang-format

--- content.js
+++ content.js
@@ -1,26 +1,35 @@
 
 var id = 0;
 
-blurIt = () => {
-    clearTimeout(id);
-    
-    let chatList = document.getElementById("side");
-    chatList ? chatList.classList.add("blurrit-class", "blur-10") : "";
-    
-    let header = document.getElementsByTagName('header')[1];
-    header ? header.classList.add("blurrit-class", "blur-4") : "";
-    
-    let taggedNameList = document.getElementsByClassName("matched-mention");
-    for (item of taggedNameList) {item.classList.add("blurrit-class", "blur-4");}
+blurIt =
+    () => {
+      clearTimeout(id);
 
-    for(i = 0; i < 256; ++i) {
-        let potentialListOfUserNameColors = document.getElementsByClassName(`color-${i+1}`);
-        let potentialListOfUserNameBgColors = document.getElementsByClassName(`bg-color-${i+1}`);
-        for (item of potentialListOfUserNameColors) {item.classList.add("blurrit-class", "blur-4");}
-        for (item of potentialListOfUserNameBgColors) {item.classList.add("blurrit-class", "blur-4");}
+      let chatList = document.getElementById("side");
+      chatList ? chatList.classList.add("blurrit-class", "blur-10") : "";
+
+      let header = document.getElementsByTagName('header')[1];
+      header ? header.classList.add("blurrit-class", "blur-4") : "";
+
+      let taggedNameList = document.getElementsByClassName("matched-mention");
+      for (item of taggedNameList) {
+        item.classList.add("blurrit-class", "blur-4");
+      }
+
+      for (i = 0; i < 256; ++i) {
+        let potentialListOfUserNameColors =
+            document.getElementsByClassName(`color-${i + 1}`);
+        let potentialListOfUserNameBgColors =
+            document.getElementsByClassName(`bg-color-${i + 1}`);
+        for (item of potentialListOfUserNameColors) {
+          item.classList.add("blurrit-class", "blur-4");
+        }
+        for (item of potentialListOfUserNameBgColors) {
+          item.classList.add("blurrit-class", "blur-4");
+        }
+      }
+
+      id = setTimeout(blurIt, 1000);
     }
-    
-    id = setTimeout(blurIt, 1000);
-}
 
 blurIt();
